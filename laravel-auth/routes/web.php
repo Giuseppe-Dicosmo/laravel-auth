c61291f515938b24cafbcdea5340f2b3aa8803cb
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-05-02 18:53:14 +0200

    autentificazione

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->name('admin.')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
